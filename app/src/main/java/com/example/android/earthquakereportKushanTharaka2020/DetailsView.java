276bdb69eb0f9a7f96f61b5259e291e507d4eb0f
tharaka <tharakakushan@gmail.com>
2020-05-29 22:27:06 +0530

    Create new java class for additional data,that we want to show in the interface

---
+++ app/src/main/java/com/example/android/earthquakereportKushanTharaka2020/DetailsView.java
@@ -0,0 +1,25 @@
+package com.example.android.earthquakereportKushanTharaka2020;
+
+public class DetailsView {
+    private int magnitude;
+    private String location;
+    private String date;
+
+    public DetailsView(int magnitude, String location, String date) {
+        this.magnitude = magnitude;
+        this.location = location;
+        this.date = date;
+    }
+
+    public int getMagnitude() {
+        return magnitude;
+    }
+
+    public String getLocation() {
+        return location;
+    }
+
+    public String getDate() {
+        return date;
+    }
+}
