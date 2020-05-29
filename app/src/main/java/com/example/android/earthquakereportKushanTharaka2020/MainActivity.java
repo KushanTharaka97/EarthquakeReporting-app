834750b1b60a4cd7e9170d0e339a59dda326f85e
tharaka <tharakakushan@gmail.com>
2020-05-29 20:12:46 +0530

    Created a ArrayList to hold some fake locations

--- app/src/main/java/com/example/android/earthquakereportKushanTharaka2020/MainActivity.java
+++ app/src/main/java/com/example/android/earthquakereportKushanTharaka2020/MainActivity.java
@@ -4,11 +4,26 @@ import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
 
+import java.util.ArrayList;
+
 public class MainActivity extends AppCompatActivity {
+
+    public static final String LOG_TAG =MainActivity.class.getName();
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.earthquake_activity);
+
+        // Create a fake list of earthquake locations.
+        ArrayList<String> earthquakes = new ArrayList<>();
+        earthquakes.add("San Francisco");
+        earthquakes.add("London");
+        earthquakes.add("Tokyo");
+        earthquakes.add("Mexico City");
+        earthquakes.add("Moscow");
+        earthquakes.add("Rio de Janeiro");
+        earthquakes.add("Paris");
+
     }
 }
