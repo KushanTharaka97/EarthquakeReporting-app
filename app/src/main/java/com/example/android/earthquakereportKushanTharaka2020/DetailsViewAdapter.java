781a0eb032d7fe69a9341a41fc27a7373fdc4ba9
tharaka <tharakakushan@gmail.com>
2020-05-30 05:12:41 +0530

    Done set Out XML to java classes We have three classes and new XML to put data those got from the classes.

--- app/src/main/java/com/example/android/earthquakereportKushanTharaka2020/DetailsViewAdapter.java
+++ app/src/main/java/com/example/android/earthquakereportKushanTharaka2020/DetailsViewAdapter.java
@@ -5,6 +5,7 @@ import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.ArrayAdapter;
+import android.widget.TextView;
 
 import java.util.ArrayList;
 
@@ -22,6 +23,22 @@ public class DetailsViewAdapter extends ArrayAdapter<DetailsView> {
                 listItemView = LayoutInflater.from(getContext()).inflate(R.layout.list_item, parent, false);
             }
 
+
+        // Get the {@link AndroidFlavor} object located at this position in the list
+        DetailsView currentDetailsView=getItem(position);
+
+        // Find the TextView in the list_item.xml layout with the ID version_name
+        TextView nameTextView = (TextView) listItemView.findViewById(R.id.mag);
+
+        nameTextView.setText(currentDetailsView.getMagnitude());
+
+        TextView locationText = (TextView) listItemView.findViewById(R.id.location);
+
+        locationText.setText(currentDetailsView.getLocation());
+
+        TextView dateTextView = (TextView) listItemView.findViewById(R.id.date);
+
+        dateTextView.setText(currentDetailsView.getDate());
 
 
         return listItemView;
