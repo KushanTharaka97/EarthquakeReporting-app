3a5e3bb7522dad3a4ea8a6b3843f892bb64a320e
tharaka <tharakakushan@gmail.com>
2020-05-30 04:54:30 +0530

    Setting new view style through declaring classes

---
+++ app/src/main/java/com/example/android/earthquakereportKushanTharaka2020/DetailsViewAdapter.java
@@ -0,0 +1,25 @@
+package com.example.android.earthquakereportKushanTharaka2020;
+
+import android.app.Activity;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.ArrayAdapter;
+
+import java.util.ArrayList;
+
+public class DetailsViewAdapter extends ArrayAdapter<DetailsView> {
+    private static final String LOG_TAG =DetailsViewAdapter.class.getSimpleName();
+
+    public DetailsViewAdapter(Activity context, ArrayList<DetailsView>earthquakes) {
+        super(context,0,earthquakes);
+    }
+
+    @Override
+    public View getView(int position, View convertView, ViewGroup parent ){
+            View listItemView = convertView;
+            if(listItemView == null){
+                listItemView = LayoutInflater.from(getContext()).inflate(R.layout.list_item, parent, false);
+            }
+    }
+}
--- app/src/main/java/com/example/android/earthquakereportKushanTharaka2020/MainActivity.java
+++ app/src/main/java/com/example/android/earthquakereportKushanTharaka2020/MainActivity.java
@@ -18,23 +18,29 @@ public class MainActivity extends AppCompatActivity {
         setContentView(R.layout.earthquake_activity);
 
         // Create a fake list of earthquake locations.
-        ArrayList<String> earthquakes = new ArrayList<>();
-        earthquakes.add("San Francisco");
-        earthquakes.add("London");
-        earthquakes.add("Tokyo");
-        earthquakes.add("Mexico City");
-        earthquakes.add("Moscow");
-        earthquakes.add("Rio de Janeiro");
-        earthquakes.add("Paris");
+        ArrayList<DetailsView> earthquakes = new ArrayList<DetailsView>();
+        earthquakes.add(new DetailsView(10,"San Francisco","Feb 2, 2019"));
+        earthquakes.add(new DetailsView(14,"London","Feb 6, 2019"));
+        earthquakes.add(new DetailsView(16,"Tokyo","Feb 4, 2019"));
+        earthquakes.add(new DetailsView(77,"Mexico City","Feb 3, 2019"));
+        earthquakes.add(new DetailsView(22,"Moscow","Feb 1, 2019"));
+        earthquakes.add(new DetailsView(44,"Rio de Janeiro","Jan 5, 2019"));
+        earthquakes.add(new DetailsView(34,"Paris","Jan 4, 2019"));
+        earthquakes.add(new DetailsView(99,"San Francisco","Feb 4, 2019"));
+
+
+
 
         // Find a reference to the {@link ListView} in the layout
         ListView earthquakeListView=(ListView) findViewById(R.id.list);
 
         //Create a new String ArrayList for earthquakes | filling array with data
-        ArrayAdapter<String> adapter=new ArrayAdapter<String>(this,android.R.layout.simple_list_item_1,earthquakes);
+        //ArrayAdapter<String> adapter=new ArrayAdapter<String>(this,android.R.layout.simple_list_item_1,earthquakes);
+       // DetailsViewAdapter
+
 
         //set adapter on list view| Now list can be generate earhquake array values |that we stored
-        earthquakeListView.setAdapter(adapter);
+       // earthquakeListView.setAdapter(adapter);
 
     }
 }
