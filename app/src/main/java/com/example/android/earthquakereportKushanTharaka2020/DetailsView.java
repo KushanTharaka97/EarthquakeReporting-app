c0bbcc4aa232df36645bf784ea37bdcb63338a1d
tharaka <tharakakushan@gmail.com>
2020-06-04 10:22:49 +0530

    Changed DetailsViews magnitude into Double datatype

--- app/src/main/java/com/example/android/earthquakereportKushanTharaka2020/DetailsView.java
+++ app/src/main/java/com/example/android/earthquakereportKushanTharaka2020/DetailsView.java
@@ -1,17 +1,17 @@
 package com.example.android.earthquakereportKushanTharaka2020;
 
 public class DetailsView {
-    private String magnitude;
+    private Double magnitude;
     private String locationDetails;
     private Long date;
 
-    public DetailsView(String magnitude, String locationDetails, Long date) {
+    public DetailsView(Double magnitude, String locationDetails, Long date) {
         this.magnitude = magnitude;
         this.locationDetails = locationDetails;
         this.date = date;
     }
 
-    public String getMagnitude() {
+    public Double getMagnitude() {
         return magnitude;
     }
 
